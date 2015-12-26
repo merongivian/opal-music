65becd15d7bb63993abf09cf18368cda5624caf5
Jose Añasco <joseanasco1@gmail.com>
2015-12-25 20:18:09 -0500

    limit browser requires only for the sequence file

--- opal/music/sequence.rb
+++ opal/music/sequence.rb
@@ -1,3 +1,13 @@
+require 'native'
+
+require 'browser/audio'
+require 'browser/audio/param_schedule'
+
+require 'browser/support'
+require 'browser/event/base'
+require 'browser/delay'
+require 'browser/window'
+
 module Music
   class Sequence
 
--- opal/opal-music.rb
+++ opal/opal-music.rb
@@ -1,12 +1,2 @@
-require 'native'
-
-require 'browser/audio'
-require 'browser/audio/param_schedule'
-
-require 'browser/support'
-require 'browser/event/base'
-require 'browser/delay'
-require 'browser/window'
-
 require 'music/note'
 require 'music/sequence'
