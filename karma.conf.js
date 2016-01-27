057c19736b1b22fe68c5e9298713e636411b5daf
Jose Añasco <joseanasco1@gmail.com>
2016-01-26 19:30:30 -0500

    use latest node version on travis

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['opal-rspec'],
+        frameworks: ['opal'],
 
         // list of files / patterns to load in the browser
         files: [
